218f807a0b2e7ab2e9e65b5d4ea460fee420e83a
Sunil Sourabh <sunil.sourabh@hotmail.com>
2018-03-04 23:40:17 +0800

    commit add

---
+++ WebApiContainer/Controllers/ItemContainer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiContainer.Controllers
+{
+    [Route("api/[controller]")]
+    public class ItemContainer
+    {
+        Dictionary<string, string> _itemsCatelog = new Dictionary<string, string>();
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return _itemsCatelog.Select((x, y) => $"Key;{x.Key}, Value :{x.Value}");
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(string id)
+        {
+            string value = string.Empty;
+            if (!_itemsCatelog.TryGetValue(id, out value))
+                throw new Exception("Key not found exception");
+            return value;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public bool Post([FromQuery]string key, [FromQuery]string value)
+        {
+            if (!_itemsCatelog.TryAdd(key,  value))
+                throw new Exception("failed to add new value");
+            return true;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{key}")]
+        public bool Put(string key, [FromQuery]string value)
+        {
+            if (!_itemsCatelog.ContainsKey(key))
+                throw new Exception("Key doesnt exist");
+
+            if (!_itemsCatelog.TryAdd(key,  value))
+                throw new Exception("Key not found exception");
+
+            return true;
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{key}")]
+        public void Delete(string key)
+        {
+            if (!_itemsCatelog.ContainsKey(key))
+                throw new Exception("Key doesnt exist");
+
+            _itemsCatelog.Remove(key);
+        }
+    }
+}
